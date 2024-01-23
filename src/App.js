902ada9ddd617ceeec1f4c9452c03e13ccb1c7ce
duxante <spomenko.dukic@gmail.com>
2024-01-23 19:41:41 +0100

    Creating feat-01-routing branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomeView from './Features/Home/homeView';
+import Navigation from './Features/Navigation/navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar selectedProducts={selectedProducts} />
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/navigation" element={<Navigation />} />
+        <Route />
+      </Routes>
     </div>
   );
 }
