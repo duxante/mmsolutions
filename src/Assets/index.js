83cf66abd8d19efa0f92f2d0739ad5d8dad15277
duxante <spomenko.dukic@gmail.com>
2024-01-28 20:34:32 +0100

    adding homeView

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -1,7 +1,14 @@
 import mms_logo from './mms_logo.png';
+import homeViewImage from './homeViewImage.png';
 
 const MmsLogo = () => {
   return <img className="mmsLogo" src={mms_logo} alt="mms logo" />;
 };
 
-export { MmsLogo };
+const HomeViewImage = () => {
+  return (
+    <img className="homeViewImage" src={homeViewImage} alt="womans hair" />
+  );
+};
+
+export { MmsLogo, HomeViewImage };
--- src/Features/Home/homeView.jsx
+++ src/Features/Home/homeView.jsx
@@ -1,12 +1,22 @@
 import { Link } from 'react-router-dom';
+import './homeView.style.css';
+import { HomeViewImage } from '../../Assets';
 
 const HomeView = () => {
   return (
-    <>
-      <Link to="/">
-        <p>Home View 123</p>
-      </Link>
-    </>
+    <div className="homeViewHolder">
+      <div className="homeViewMain">
+        {/* <Link to="/">
+          <p>Home View 123</p>
+        </Link> */}
+
+        <h2>Elevate Your Success: Expert Counseling Insights.</h2>
+        <div className="nameAndImage">
+          <h1>Marija Milićević</h1>
+          <HomeViewImage />
+        </div>
+      </div>
+    </div>
   );
 };
 
