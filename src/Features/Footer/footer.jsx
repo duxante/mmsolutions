104f6df7c0b90be1d51205ae8b887f808156aea0
duxante <spomenko.dukic@gmail.com>
2024-01-30 18:33:26 +0100

    test test

--- src/Features/Footer/footer.jsx
+++ src/Features/Footer/footer.jsx
@@ -2,71 +2,10 @@ const Footer = () => {
   return (
     <>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius aut
-        doloremque minus officia quibusdam, quasi dolor perspiciatis culpa!
-        Laudantium, animi optio autem maiores quas corrupti odio quasi
-        voluptatem inventore architecto. Lorem Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Asperiores, maiores eum quia
-        exercitationem saepe ex vitae aspernatur vel in ipsam repellendus enim
-        reiciendis laboriosam aut, totam, minima impedit neque blanditiis? Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolores
-        sunt sapiente veniam? Ipsam, fugit! Fugit eos nostrum earum. Eveniet
-        possimus odio ullam placeat nam. Facilis iure ullam saepe magnam. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Sunt aliquid quisquam
-        ipsum consectetur delectus consequuntur temporibus laborum reprehenderit
-        pariatur, ea corporis quidem quasi iusto, minus alias accusamus, ab fuga
-        modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-        sapiente itaque? Veniam illum omnis necessitatibus quasi modi odio,
-        officiis quia beatae eos possimus voluptatibus, fugiat esse porro in?
-        Vel, ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Eius aut doloremque minus officia quibusdam, quasi dolor perspiciatis
-        culpa! Laudantium, animi optio autem maiores quas corrupti odio quasi
-        voluptatem inventore architecto. Lorem Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Asperiores, maiores eum quia
-        exercitationem saepe ex vitae aspernatur vel in ipsam repellendus enim
-        reiciendis laboriosam aut, totam, minima impedit neque blanditiis? Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolores
-        sunt sapiente veniam? Ipsam, fugit! Fugit eos nostrum earum. Eveniet
-        possimus odio ullam placeat nam. Facilis iure ullam saepe magnam. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Sunt aliquid quisquam
-        ipsum consectetur delectus consequuntur temporibus laborum reprehenderit
-        pariatur, ea corporis quidem quasi iusto, minus alias accusamus, ab fuga
-        modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-        sapiente itaque? Veniam illum omnis necessitatibus quasi modi odio,
-        officiis quia beatae eos possimus voluptatibus, fugiat esse porro in?
-        Vel, ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Eius aut doloremque minus officia quibusdam, quasi dolor perspiciatis
-        culpa! Laudantium, animi optio autem maiores quas corrupti odio quasi
-        voluptatem inventore architecto. Lorem Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Asperiores, maiores eum quia
-        exercitationem saepe ex vitae aspernatur vel in ipsam repellendus enim
-        reiciendis laboriosam aut, totam, minima impedit neque blanditiis? Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolores
-        sunt sapiente veniam? Ipsam, fugit! Fugit eos nostrum earum. Eveniet
-        possimus odio ullam placeat nam. Facilis iure ullam saepe magnam. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Sunt aliquid quisquam
-        ipsum consectetur delectus consequuntur temporibus laborum reprehenderit
-        pariatur, ea corporis quidem quasi iusto, minus alias accusamus, ab fuga
-        modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-        sapiente itaque? Veniam illum omnis necessitatibus quasi modi odio,
-        officiis quia beatae eos possimus voluptatibus, fugiat esse porro in?
-        Vel, ipsum? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Eius aut doloremque minus officia quibusdam, quasi dolor perspiciatis
-        culpa! Laudantium, animi optio autem maiores quas corrupti odio quasi
-        voluptatem inventore architecto. Lorem Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Asperiores, maiores eum quia
-        exercitationem saepe ex vitae aspernatur vel in ipsam repellendus enim
-        reiciendis laboriosam aut, totam, minima impedit neque blanditiis? Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolores
-        sunt sapiente veniam? Ipsam, fugit! Fugit eos nostrum earum. Eveniet
-        possimus odio ullam placeat nam. Facilis iure ullam saepe magnam. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Sunt aliquid quisquam
-        ipsum consectetur delectus consequuntur temporibus laborum reprehenderit
-        pariatur, ea corporis quidem quasi iusto, minus alias accusamus, ab fuga
-        modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-        sapiente itaque? Veniam illum omnis necessitatibus quasi modi odio,
-        officiis quia beatae eos possimus voluptatibus, fugiat esse porro in?
-        Vel, ipsum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, et,
+        soluta velit odio repudiandae tempore esse, quod veniam asperiores
+        nostrum dolore molestias quibusdam voluptatem maxime non eveniet
+        excepturi ipsum ut!
       </p>
     </>
   );
