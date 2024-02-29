b72cbabca1fefb0ddc8aed0667ddccb9102e7e3b
duxante <spomenko.dukic@gmail.com>
2024-02-29 18:44:39 +0100

    paragraph

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/* import HomeView from './features/home/homeView'; */
+/* import HomeView from './features/home/homeView';
 import Navigation from './features/navigation/components/navigation';
 import Footer from '../src/features/footer/footer';
 import About from './features/about/components/about';
@@ -6,21 +6,22 @@ import ServiceOffering from './features/serviceOffering/serviceOffering';
 import Statistics from './features/statistics/statistics';
 import Testimonials from './features/testimonials/testimonials';
 import BlogPosts from './features/blog/blogPosts';
-import MadeBy from './features/madeBy/madeBy';
+import MadeBy from './features/madeBy/madeBy'; */
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      {/* <HomeView /> */}
+      {/* <Navigation />
+      <HomeView />
       <About />
       <ServiceOffering />
       <Statistics />
       <Testimonials />
       <BlogPosts />
       <Footer />
-      <MadeBy />
+      <MadeBy /> */}
+      <p>TEST</p>
     </div>
   );
 }
